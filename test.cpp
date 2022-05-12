3d26948498c8efcdc0e428d0f8bdffea1fc02920
hassanraza13131 <105425687+hassanraza13131@users.noreply.github.com>
2022-05-12 17:24:40 +0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -16,5 +16,13 @@ void f() {
 
 int main()
 {
-  a = "hasan@gmail.com"
+	a = "hasan@gmail.com";
+	float i = 0.0f;
+	for (i = 0; i < 1000000.0f; i++) {
+		cout<< i;
+		
+	}
+	for (i = 0; i < 100000000.0f; i++) { 
+		cout<< i;
+	}
 }
